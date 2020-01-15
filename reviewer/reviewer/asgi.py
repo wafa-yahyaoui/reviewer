8261695f27788daa694c9cc95399b8b25de4ff90
“Wafa <wafa.yahyaoui@airgreets.com>
2020-01-15 08:48:58 +0100

    Created project

---
+++ reviewer/reviewer/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for reviewer project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviewer.settings')
+
+application = get_asgi_application()
